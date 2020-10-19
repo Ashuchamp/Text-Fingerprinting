3537fac6bae1c1bacca21646b911bd0401a20d0b
Daniel Pyon <daniel@quicksat.com>
2020-10-19 13:53:50 -0700

    Basic menu

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
+
+import {
+	Navbar,
+	Nav,
+	NavDropdown,
+	Form,
+	FormControl,
+	Button
+} from 'react-bootstrap';
+
+import Home from './Home.js';
+import About from './About.js';
+import Demo from './Demo.js';
+
+import Logo from './logo.svg';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<Navbar bg="light" expand="lg">
+  					<Navbar.Brand href="#home">
+						<img
+							src={ Logo }
+							width="30"
+							height="30"
+							className="d-inline-block align-top"
+							alt="Writeprint Logo"
+						/>{' '}
+						Writeprint
+					</Navbar.Brand>
+  						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+  						<Navbar.Collapse id="basic-navbar-nav">
+    						<Nav className="mr-auto">
+      							<Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+      							<Nav.Link href="#demo"><Link to="/demo">Demo</Link></Nav.Link>
+								<Nav.Link href="#about"><Link to="/about">About</Link></Nav.Link>
+      							<NavDropdown title="More" id="basic-nav-dropdown">
+        							<NavDropdown.Item href="#action/3.1">For Developers</NavDropdown.Item>
+        							<NavDropdown.Item href="#action/3.2">Source Code</NavDropdown.Item>
+        						<NavDropdown.Divider />
+        						<NavDropdown.Item href="#action/3.4">Team</NavDropdown.Item>
+      						</NavDropdown>
+    					</Nav>
+  					</Navbar.Collapse>
+				</Navbar>
+				<Switch>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/demo">
+						<Demo />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
